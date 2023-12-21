468ed714a1bd0d6cc1843a276e599aa4dc4ab541
Ayres Flesch <ayresabf@gmail.com>
2023-12-21 10:49:53 -0300

    day 9 part 2

---
+++ day-9/day-9-part-2.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+
+fs.readFile('input.txt', 'utf-8', function (err, data) {
+    const history = data.split(/\r?\n/)
+
+    const sum = history.reduce((acc, h) => {
+        const numbers = h.split(' ').map(n => parseInt(n, 10)).reverse()
+
+        let lines = [numbers]
+        let currentLine = numbers
+
+        while (true) {
+            const line = []
+            for (let i = 0; i < currentLine.length - 1; i++) {
+                line.push(currentLine[i + 1] - currentLine[i])
+            }
+
+            currentLine = line
+            lines.push(line)
+
+            if (line.every(l => l === 0) || line.length === 1) {
+                break
+            }
+        }
+
+        for (let i = lines.length - 2; i >= 0; i--) {
+            lines[i].push(lines[i + 1][lines[i + 1].length - 1] + lines[i][lines[i].length - 1])
+        }
+
+        return acc + lines[0][lines[0].length - 1];
+
+    }, 0);
+
+    console.log(sum);
+})
